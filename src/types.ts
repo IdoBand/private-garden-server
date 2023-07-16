64a1f58347680d9b38de19a53d459270b37756b9
unknown <ido.bandd@gmail.com>
2023-07-16 18:40:44 +0300

    types

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
 export interface PlantEdit {
-    plantName: string,
-    img: string | { data: Buffer | []; contentType: string }
+    plantName?: string,
+    img?: string | { data: Buffer | []; contentType: string }
 }
 export interface PlantUpdate {
-    plantId: string
-    plantName: string
-    dateAdded: string
-    img: string | { data: Buffer | []; contentType: string }
-    irrigation: Irrigation
-    notes: string
+    plantId?: string
+    plantName?: string
+    dateAdded?: string
+    img?: string | { data: Buffer | []; contentType: string }
+    irrigation?: Irrigation
+    notes?: string
 }
 export interface Irrigation {
-    boolean: boolean
+    boolean?: boolean
     waterQuantity?: number
     fertilizer?: string
     fertilizerQuantity?: number
